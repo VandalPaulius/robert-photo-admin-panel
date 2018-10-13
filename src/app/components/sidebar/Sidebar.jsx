4f6e79b844a0288c0e13dd9f2d0ab311bb06d5e2
Vandal Paulius Juska <aki.maxis@gmail.com>
2018-10-13 11:35:23 +0100

    Updated chevron icon rotate

--- src/app/components/sidebar/Sidebar.jsx
+++ src/app/components/sidebar/Sidebar.jsx
@@ -15,27 +15,67 @@ class Sidebar extends React.Component {
         };
 
         this.actions = this.initActions();
+
+        this.windowResizeListener = this.windowResizeListener.bind(this);
     }
 
+    // eslint-disable-next-line react/sort-comp
     initActions() {
         return {
             toggleMenuOpen: (open) => {
                 this.setState({ open });
             },
+            updateOpenCloseEnabled: (openCloseEnabled) => {
+                this.setState({ openCloseEnabled });
+            },
+            // updateSidebarOpen: (data) => {
+            //     console.log('data: ', data)
+            // },
         };
     }
 
+    componentDidMount() {
+        this.windowResizeListener()
+        window.addEventListener('resize', this.windowResizeListener);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.windowResizeListener);
+    }
+
+    windowResizeListener() {
+        const page = document.documentElement;
+        if (page.offsetWidth < 800) {
+            if (!this.state.openCloseEnabled) {
+                this.actions.updateOpenCloseEnabled(true);
+            }
+            if (this.state.open) {
+                this.actions.toggleMenuOpen(false);
+            }
+        } else if (this.state.openCloseEnabled) {
+            this.actions.updateOpenCloseEnabled(false);
+        }
+    }
+
     render() {
-        console.log('Sidebar this.props: ', this.props)
+        console.log('Sidebar this.state: ', this.state)
 
         return (
             <div className={styles.sidebar}>
                 <div className={styles.buttonContainer}>
                     <div
-                        className={styles.openCloseButton}
+                        className={`${
+                            styles.openCloseButton
+                        } ${
+                            this.state.openCloseEnabled ? '' : 'hide'
+                        }`}
                         tabIndex={0}
                         role="button"
-                        onClick={() => this.actions.toggleMenuOpen(!this.state.open)}
+                        onClick={() => {
+                            if (this.state.openCloseEnabled) {
+                                this.actions.toggleMenuOpen(!this.state.open);
+                            }
+                        }}
                     >
                         <ChevronIcon
                             className={`${
